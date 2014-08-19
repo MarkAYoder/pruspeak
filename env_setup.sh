75cd2ec524056d5b2434a147471b9edf9baa8f38
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-08-19 13:09:20 -0400

    Added echo's to remove HDMI nad P9_41 and P9_42

--- env_setup.sh
+++ env_setup.sh
@@ -1,3 +1,9 @@
 insmod ./bin/pru_speak.ko
-echo BB-PRUSPEAK > /sys/devices/bone_capemgr.9/slots
-python src/userspace_lib/bs_fifo.py > ~/.pruspeak.log &
+SLOTS=/sys/devices/bone_capemgr.*/slots
+# Turn off HDMI and P9_41 and P9_42
+echo -6 > $SLOTS
+echo -7 > $SLOTS
+echo -8 > $SLOTS
+# Turn on pruspeak
+echo BB-PRUSPEAK > $SLOTS
+python src/userspace_lib/bs_fifo.py > pruspeak.log &
