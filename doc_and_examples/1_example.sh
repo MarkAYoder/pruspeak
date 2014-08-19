337f00f99d1e45ba232e8ee23470cf2764ae5409
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2014-08-19 15:50:54 -0400

    Playing with duty cycle

--- doc_and_examples/1_example.sh
+++ doc_and_examples/1_example.sh
@@ -1,8 +1,9 @@
+echo DEBUG > /dev/pruspeak
 echo SCRIPT > /dev/pruspeak
 echo SET DIO[0], 1 > /dev/pruspeak
-echo WAIT 3 > /dev/pruspeak
+echo WAIT 2 > /dev/pruspeak
 echo SET DIO[0], 0 > /dev/pruspeak
-echo WAIT 1 > /dev/pruspeak
+echo WAIT 2 > /dev/pruspeak
 echo GOTO 0 > /dev/pruspeak
 echo ENDSCRIPT > /dev/pruspeak
 echo RUN > /dev/pruspeak
--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 cp ./bin/pru0_firmware /lib/firmware/
 cp ./bin/pru1_firmware /lib/firmware/
 cp ./bin/BB-PRUSPEAK-00A0.dtbo /lib/firmware/
-
